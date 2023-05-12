f1cb72e22581158bfbe6f06586a03e0a77c50288
Mikołaj Bisewski <s188594@o365.student.pg.edu.pl>
2023-05-12 17:47:09 +0200

    Implemented client logic, shutdown, connecting and sending messages to server

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,23 +1,65 @@
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client implements Runnable{
     private Socket clientSock;
+    private ObjectOutputStream out;
+    private ObjectInputStream in;
+    private Scanner sc;
     private String nickname;
     private Integer n;
 
+    public Client(){
+        sc = new Scanner(System.in);
+    }
+
     @Override
     public void run() {
-        try (Socket client = new Socket("127.0.0.1",9999)){
+        try (Socket client = new Socket("127.0.0.1",9999);
+             ObjectOutputStream oos = new ObjectOutputStream(client.getOutputStream());
+             ObjectInputStream ois = new ObjectInputStream(client.getInputStream())){
             this.clientSock = client;
+            this.in = ois;
+            this.out = oos;
+            String msg = (String) in.readObject();
+            System.out.println(msg);
+            Integer n = sc.nextInt();
+            out.writeObject(sc);
+            msg = (String) in.readObject();
+            System.out.println(msg);
+            //TODO: send n messages in message class type
+
+            msg = (String) in.readObject();
+            System.out.println(msg);
+            System.out.println("finished sending contents to server, closing connection");
+            shutDown();
 
         } catch (IOException e){
             //TODO: handle exception
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            //TODO: handle exception
+            e.printStackTrace();
         }
     }
 
+    public void shutDown() {
+        try{
+            this.in.close();
+            this.out.close();
+            if(!clientSock.isClosed()){
+                clientSock.close();
+            }
+        }catch (IOException e){
+            //ignore exception
+        }
+    }
 
     public static void main(String[] args) {
-
+        Client client = new Client();
+        client.run();
     }
 }
