6440c498b24f7ed05c2d1e68b324841ee8b71e67
Mikołaj Bisewski <s188594@o365.student.pg.edu.pl>
2023-05-12 17:05:07 +0200

    Implemented run method. connected to host

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,4 +1,22 @@
-public class Client {
+import java.io.IOException;
+import java.net.Socket;
+
+public class Client implements Runnable{
+    private Socket clientSock;
+    private String nickname;
+    private Integer n;
+
+    @Override
+    public void run() {
+        try (Socket client = new Socket("127.0.0.1",9999)){
+            this.clientSock = client;
+
+        } catch (IOException e){
+            //TODO: handle exception
+        }
+    }
+
+
     public static void main(String[] args) {
 
     }
